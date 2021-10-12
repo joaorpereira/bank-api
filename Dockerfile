cd12472ca175443896eb432b41846c50b8467e12
joaorpereira <jprp17@live.com>
2021-10-11 22:43:21 -0300

    feature: remove docker

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:alpine
-WORKDIR /app
-COPY package.json .
-RUN npm install
-COPY . ./
-ENV PORT 3003
-EXPOSE ${PORT}
-CMD npm run dev
--- src/database/DataBase.ts
+++ src/database/DataBase.ts
@@ -1,14 +1,16 @@
+import dotenv from "dotenv";
 import knex from "knex";
 
+dotenv.config();
 export default class DataBase {
   protected static connection = knex({
     client: "mysql",
     connection: {
-      host: "localhost",
-      port: 3308,
-      user: "root",
-      password: "password",
-      database: "bank_database",
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_LOCAL_PORT),
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
     },
   });
 }
